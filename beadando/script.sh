00db0be5639671d8412669812f6a0879021fa2a9
vbence121 <vbence121@gmail.com>
2019-04-18 17:58:59 +0200

    asd

--- beadando/script.sh
+++ beadando/script.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
-for i in ~/Music/*.mp3
+
+	case $# in 
+		1)
+		    from="~/Music/"
+		    to=~/Music/
+	          case $1 in 
+			-G)
+				G=true
+				;;
+			-h | --help | *)
+				echo "Syntax guide: ./script.sh [from] [to] [-G]"
+			#	echo "If one or less location arguments are provided the default ~/Music folder will be used"
+				exit 0
+				;;
+		  esac
+		  ;;
+		2) 
+			if [ -d $1 & -d $2 ]; then
+				from=$1
+				to=$2
+			else 
+				./$0 -h
+				exit 0
+			fi
+			;;	
+		3)	
+			if [[ -d $1 && -d $2 && $3 = "-G" ]]
+				from="$1"
+				to="$2"/
+				G=true
+			else
+				./$0 -h
+				exit 0
+			fi
+			;;
+		4 | *)
+				./$0 -h
+				exit 0
+			;;
+	esac
+for i in "$from"/*.mp3
 do
+    if [ $G = true ]; then
+ 	genre=`ffprobe "$i" 2>&1 | grep -w "genre" | cut -c 23-`
+	to="$to"/"$genre"
+    fi
 	echo $i
 	Artist=`ffprobe "$i" 2>&1 | grep -w "artist" | cut -c 23-`
 	echo $Artist
-	mkdir ~/Music/"$Artist" 2> /dev/null
+	mkdir "$to"/"$Artist" 2> /dev/null
 	Album=`ffprobe "$i" 2>&1 | grep  -w "album" | cut -c 23-`
 	echo $Album
-	mkdir ~/Music/"$Artist"/"$Album" 2> /dev/null
+	mkdir "$to"/"$Artist"/"$Album" 2> /dev/null
 	Title=`ffprobe "$i" 2>&1 | grep -w  "Title"  | cut -c 23-`
 	echo $Title
-	cp "$i" ~/Music/"$Artist"/"$Album"/"$Title"
+	cp "$i" "$to"/"$Artist"/"$Album"/"$Title"
 done
